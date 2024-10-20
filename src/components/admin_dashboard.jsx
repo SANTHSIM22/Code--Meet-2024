8c4c012f30152b8de614d5a5bb145d5b9a83bae0
RagedRebel <reubenav1305@gmail.com>
2024-10-20 16:33:03 +0530

    modified table in dashboard

--- src/components/admin_dashboard.jsx
+++ src/components/admin_dashboard.jsx
@@ -31,91 +31,100 @@ const Dashboard = () => {
   };
 
   return (
-    <div className=" flex  min-h-screen  items-center justify-center bg-white p-4">
-      <div className="flex flex-col w-screen h-screen justify-start items-center">
-      <h1 className="text-3xl font-bold h-60 text-gray-900 mb-4 align-text-top ">Dashboard</h1>
-      <table className='table-auto border border-collapse border-slate-400'>
+    <div className=" flex  min-h-screen  items-center justify-left bg-white p-4">
+      <div className="flex flex-col  w-screen h-screen justify-start items-start">
+      <h1 className="text-3xl font-bold h-36 text-gray-900 mb-4  ">Dashboard</h1>
+      <label className='font-medium text-xl'>Saved Tests</label>
+      <table className='table-auto border  border-slate-400 w-11/12 h-44 bg-slate-200'>
         <thead>
           <tr>
-            <th className='text-2xl  border border-slate-400'>Test No.</th>
-            <th className="border border-slate-400"> 
-     
-         
-
-        </th>
+            <th className='text-2xl  border border-slate-400 bg-slate-500'>Test No.</th>
+           
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="font-medium text-center  border border-slate-400">
-              Test 1
+        <tr className="font-medium text-center ">
+            <td className=" border border-slate-400 bg-slate-500 ">
+            {testCode}
             </td>
-            <td className="flex justify-around   items-center  border border-slate-400">
+            <td className="border border-slate-400 ">
             <button
             onClick={handleStartTest}
-            className="bg-green-500 text-white p-2 w-40 rounded"
+            className="bg-green-500 text-white p-2 w-40 rounded hover:bg-green-700"
           >
             Start Test
           </button>
+          </td>
+          <td className="border border-slate-400">
           <button
             onClick={handleEndTest}
-            className="bg-red-500 text-white p-2  w-40 rounded"
+            className="bg-red-500 text-white p-2  w-40 rounded hover:bg-red-700"
           >
             End Test
           </button>
+          </td>
+          <td className="border border-slate-400">
           <button
             onClick={handleResetTest}
-            className="bg-yellow-500 text-white w-40 p-2 rounded"
+            className="bg-yellow-400 text-white w-40 p-2 rounded hover:bg-yellow-700"
           >
             Reset Test
           </button>
             </td>
           </tr>
           <tr className="font-medium text-center">
-            <td className=" border border-slate-400">
-              Test 2
+            <td className=" border border-slate-400 bg-slate-500">
+            {testCode}
             </td>
-            <td className="flex justify-around   items-center  border border-slate-400">
+            <td className="border border-slate-400 ">
             <button
             onClick={handleStartTest}
-            className="bg-green-500 text-white p-2 w-40 rounded"
+            className="bg-green-500 text-white p-2 w-40 rounded hover:bg-green-700"
           >
             Start Test
           </button>
+          </td>
+          <td className="border border-slate-400">
           <button
             onClick={handleEndTest}
-            className="bg-red-500 text-white p-2  w-40 rounded"
+            className="bg-red-500 text-white p-2  w-40 rounded hover:bg-red-700"
           >
             End Test
           </button>
+          </td>
+          <td className="border border-slate-400">
           <button
             onClick={handleResetTest}
-            className="bg-yellow-500 text-white w-40 p-2 rounded"
+            className="bg-yellow-400 text-white w-40 p-2 rounded hover:bg-yellow-700"
           >
             Reset Test
           </button>
             </td>
           </tr>
           <tr className="font-medium text-center">
-            <td className=" border border-slate-400">
-              Test 3
+            <td className=" border border-slate-400 bg-slate-500">
+            {testCode}
             </td>
-            <td className="flex justify-around   border border-slate-400 items-center">
+            <td className="border border-slate-400 ">
             <button
             onClick={handleStartTest}
-            className="bg-green-500 text-white p-2 w-40 rounded"
+            className="bg-green-500 text-white p-2 w-40 rounded hover:bg-green-700"
           >
             Start Test
           </button>
+          </td>
+          <td className="border border-slate-400">
           <button
             onClick={handleEndTest}
-            className="bg-red-500 text-white p-2  w-40 rounded"
+            className="bg-red-500 text-white p-2  w-40 rounded hover:bg-red-700"
           >
             End Test
           </button>
+          </td>
+          <td className="border border-slate-400">
           <button
             onClick={handleResetTest}
-            className="bg-yellow-500 text-white w-40 p-2 rounded"
+            className="bg-yellow-400 text-white w-40 p-2 rounded hover:bg-yellow-700"
           >
             Reset Test
           </button>
